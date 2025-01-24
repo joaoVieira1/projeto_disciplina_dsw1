6d99abd9c5132a9230453f1d62285128aaf0dd69
João Pedro da Silva Vieira AQ3022366 <vieira.joao1@aluno.ifsp.edu.br>
2025-01-23 22:32:39 -0300

    configuração da pool de conexões

--- src/main/java/br/edu/ifsp/dsw1/model/dao/connection/DatabaseConnection.java
+++ src/main/java/br/edu/ifsp/dsw1/model/dao/connection/DatabaseConnection.java
@@ -1,5 +1,25 @@
 package br.edu.ifsp.dsw1.model.dao.connection;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class DatabaseConnection {
 
+	private static final String RESOURCE = "java:/comp/env/jdbc/mysql";
+
+	public static Connection getConnection() throws SQLException {
+		try {
+			InitialContext context = new InitialContext();
+			DataSource dataSource = (DataSource) context.lookup(RESOURCE);
+			return dataSource.getConnection();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 }
